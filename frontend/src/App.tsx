4c12ff6abed288a0a22a0e36fc9e4ca67f9802c7
Keshavcpa01 <keshavcpa@gmail.com>
2025-05-09 18:40:15 -0500

    Remove nested <Router> from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -25,7 +26,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <Router>
+   <>
       <ToastContainer position="top-center" autoClose={3000} />
 
 
@@ -65,7 +66,7 @@ const App: React.FC = () => {
           </Routes>
         </div>
       </div>
-    </Router>
+    </>
   );
 };
 
